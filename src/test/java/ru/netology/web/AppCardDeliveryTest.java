07571a03484c435c8da408ff793a5afa7094e7aa
Yana Vitkovskaya <vitkovskaya.yana@gmail.com>
2023-03-11 19:41:53 +0300

    Fix

--- src/test/java/ru/netology/web/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/web/AppCardDeliveryTest.java
@@ -1,21 +1,18 @@
 package ru.netology.web;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static java.util.concurrent.TimeUnit.SECONDS;
+
 
 public class AppCardDeliveryTest {
 
@@ -24,32 +21,10 @@ public class AppCardDeliveryTest {
 
     }
 
-    private WebDriver driver;
-
-    @BeforeAll
-    public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-gpu");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(15, SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(15, SECONDS);
-    }
-
-
 
     @Test
     void shouldBeSuccessful() {
+        Configuration.headless = true;
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Санкт-Петербург");
         String currentDate = generateDate(3, "dd.MM.yyyy");
